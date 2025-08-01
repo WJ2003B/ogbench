2aa365f5c75677429b17e4fa5fd445112af9d6b4
Bill Zheng <132519771+WJ2003B@users.noreply.github.com>
2025-07-31 17:42:21 -0700

    Update env_utils.py

--- impls/utils/env_utils.py
+++ impls/utils/env_utils.py
@@ -89,15 +89,19 @@ def make_env_and_datasets(dataset_name, dataset_path=None,cur_env=None, frame_st
     # Use compact dataset to save memory.
     if cur_env is not None:
         dataset_only = True
+        env, train_dataset, val_dataset = ogbench.make_env_and_datasets(dataset_name, dataset_path=dataset_path, cur_env=cur_env, dataset_only=dataset_only, compact_dataset=True)
+        train_dataset = Dataset.create(**train_dataset)
+        val_dataset = Dataset.create(**val_dataset)
+    
+        if frame_stack is not None:
+            env = FrameStackWrapper(env, frame_stack)
+    
+        env.reset()
+    
+        return env, train_dataset, val_dataset
     else:
         dataset_only = False
-    env, train_dataset, val_dataset = ogbench.make_env_and_datasets(dataset_name, dataset_path=dataset_path, cur_env=cur_env, dataset_only=dataset_only, compact_dataset=True)
-    train_dataset = Dataset.create(**train_dataset)
-    val_dataset = Dataset.create(**val_dataset)
-
-    if frame_stack is not None:
-        env = FrameStackWrapper(env, frame_stack)
-
-    env.reset()
-
-    return env, train_dataset, val_dataset
+        train_dataset, val_dataset = ogbench.make_env_and_datasets(dataset_name, dataset_path=dataset_path, cur_env=cur_env, dataset_only=dataset_only, compact_dataset=True)
+        train_dataset = Dataset.create(**train_dataset)
+        val_dataset = Dataset.create(**val_dataset)
+        return train_dataset, val_dataset
